034e4cf2091d63822e36a01d1f6637567e694434
Joshua Peek <josh@joshpeek.com>
2012-12-09 14:51:04 -0600

    Missing cli arg error

--- cli.js
+++ cli.js
@@ -2,7 +2,13 @@
 var profile = require('./index').profile;
 var inspect = require('util').inspect;
 
-profile(process.argv[2], function(err, report) {
-  if (err) throw err;
-  console.log(inspect(report, false, 10, true));
-});
+var url = process.argv[2];
+
+if (url) {
+  profile(url, function(err, report) {
+    if (err) throw err;
+    console.log(inspect(report, false, 10, true));
+  });
+} else {
+  console.error("dom-prof <url>");
+}
