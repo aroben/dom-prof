11a9acc5f50f554de3edc1c8cef8a235e9667ac9
Joshua Peek <josh@joshpeek.com>
2014-02-12 19:24:21 -0600

    Compile coffeescript with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,20 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    coffee: {
+      all: {
+        files: {
+          'index.js': 'index.coffee',
+          'support.js': 'support.coffee'
+        }
+      }
+    },
     nodeunit: {
       all: ['test.js']
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  grunt.registerTask('test', ['nodeunit']);
+  grunt.registerTask('default', ['coffee', 'nodeunit']);
 };
--- Makefile
+++
@@ -1,9 +0,0 @@
-%.js: %.coffee
-	./node_modules/.bin/coffee -c $<
-
-node_modules:
-	npm install
-
-build: node_modules index.js support.js
-
-.PHONY: test
