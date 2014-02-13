9d1783fbfb69efda06abcc12d57c75f8c6fdd195
Joshua Peek <josh@joshpeek.com>
2014-02-12 20:06:51 -0600

    Fix cli

--- cli.js
+++ cli.js
@@ -5,11 +5,10 @@ var inspect = require('util').inspect;
 var url = process.argv[2];
 
 if (url) {
-  profile(url, function(err, report) {
-    if (err) {
-      throw err;
-    }
+  profile(url).then(function(report) {
     console.log(inspect(report, false, 10, true));
+  }, function(err) {
+    console.error(err);
   });
 } else {
   console.error('dom-prof <url>');
