ee0cf3815f180318c584b48b36de4eb60cfe1531
Joshua Peek <josh@joshpeek.com>
2014-02-17 14:37:45 -0600

    Test page with no jquery

--- test/test.js
+++ test/test.js
@@ -4,6 +4,52 @@ var http = require('http');
 var profile = require('../index').profile;
 
 module.exports = {
+  'no jquery': {
+    'profile': function(test) {
+      var server = http.createServer(function(req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile(__dirname+'/test-no-jquery.html', 'utf8', function(err, data) {
+          res.end(data);
+        });
+      }).listen(0);
+
+      profile('http://localhost:'+server.address().port).then(function(r) {
+        test.equal(7, r.dom.total);
+        test.equal(3, r.dom.maxDepth);
+        test.equal(2.4285714285714284, r.dom.averageDepth);
+        test.equal(20, r.dom.serializedSize);
+
+        test.equal(2, r.scriptTags);
+        test.equal(0, r.stylesheetLinks);
+        test.equal(0, r.inlineScripts);
+        test.equal(0, r.inlineStyles);
+        test.equal(2, r.globals.length);
+
+        test.equal(4, r.cssRules.length, JSON.stringify(r.cssRules));
+        test.equal(4, r.cssExplain.total);
+        test.equal(1, r.cssExplain.categories.id);
+        test.equal(1, r.cssExplain.categories.class);
+        test.equal(1, r.cssExplain.categories.tag);
+        test.equal(1, r.cssExplain.categories.universal);
+
+        test.equal(5, r.eventListeners.total);
+        test.equal(1, r.eventListeners.calls.DOMContentLoaded);
+        test.equal(1, r.eventListeners.calls.load);
+        test.equal(1, r.eventListeners.calls.keydown);
+        test.equal(2, r.eventListeners.calls.click);
+
+        test.equal(6, r.querySelector.total);
+        test.equal(1, r.querySelector.calls.h1);
+
+        server.close();
+        test.done();
+      }, function(err) {
+        test.ifError(err);
+        test.done();
+      });
+    }
+  },
+
   'jquery 1.8.x': {
     'profile': function(test) {
       var server = http.createServer(function(req, res) {
